10709340294b12290c6ef18ec3843f0708912dee
stefan-schlagh <stefan.schlaghuber@gmail.com>
2020-06-03 11:40:40 +0200

    added build file

---
+++ build/precache-manifest.8953b9aadf128ce5e4fd3e771100b940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fbd5a156acfa52b6bbac56b4a4fbbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5174845c40b92a0e00e9",
+    "url": "/static/css/2.e3346d4f.chunk.css"
+  },
+  {
+    "revision": "5174845c40b92a0e00e9",
+    "url": "/static/js/2.b8bb2453.chunk.js"
+  },
+  {
+    "revision": "73ee0ca5126baf4b589a8f4ac3348fa7",
+    "url": "/static/js/2.b8bb2453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1304331511b1a3c059fc",
+    "url": "/static/js/main.a8ab6144.chunk.js"
+  },
+  {
+    "revision": "f8b150b11c7cd31d5597",
+    "url": "/static/js/runtime-main.b2bde4a4.js"
+  }
+]);
