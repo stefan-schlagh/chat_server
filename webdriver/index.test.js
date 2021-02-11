849dffc23b8d72c0bcd79b2e1bd81c68905602d2
stefan-schlagh <64643082+stefan-schlagh@users.noreply.github.com>
2021-02-11 17:22:14 +0100

    add webdriver tests

--- webdriver/index.test.js
+++ webdriver/index.test.js
@@ -1,5 +1,4 @@
 const {Builder, By, Key, until,Capabilities} = require('selenium-webdriver');
-const fs = require('fs');
 require('selenium-webdriver/chrome');
 
 const rootURL = 'http://localhost:80';
@@ -14,6 +13,11 @@ const d = new Builder().withCapabilities(chromeCapabilities).build();
 const waitUntilTime = 20000;
 let driver, el, actual, expected;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5;
+
+const messages = [
+    'test 1111',
+    'test 2222'
+]
 
 //https://medium.com/@mathieux51/jest-selenium-webdriver-e25604969c6
 
@@ -33,10 +37,68 @@ it('should login',async () => {
     await driver.findElement(By.name('password')).sendKeys('password', Key.RETURN);
 });
 
-it('should click on chat',async () => {
+it('should wait until chats loaded',async () => {
     //wait until chats loaded
-    await driver.wait(until.elementLocated(By.className('chat-item')),10000);
-    //const chatList = await driver.findElements(By.className('chat-item'));
+    await driver.wait(until.elementLocated(By.className('chat-item')), 10000);
+});
+it('should click on chat of user 123',async () => {
+
+    const chatList = await driver.findElements(By.className('chat-item'));
+
+    let foundUser123 = false;
+    let usernameItem;
+    for (let i = 0; i < chatList.length; i++) {
+        // get username item
+        const usernameItemA = await chatList[i].findElements(By.className('chatItem-username'));
+        // findElements should return a array with length 1
+        expect(usernameItemA.length).toEqual(1);
+        usernameItem = usernameItemA[0];
+        // get username text
+        const usernameText = await usernameItem.getText();
+        expect(typeof usernameText).toBe('string');
+        // compare username
+        if (usernameText === 'user123') {
+            foundUser123 = true;
+            break;
+        }
+    }
+    // user should have been found
+    expect(foundUser123).toEqual(true);
+    //click on usernameItem
+    await usernameItem.click();
+
+});
+it('should wait until message loaded',async () => {
+    //wait until message loaded
+    await driver.wait(until.elementLocated(By.className('msg-container')), 10000);
+});
+it('should send messages',async () => {
+    // get message form
+    const msgForm = await driver.findElement(By.className('msg-form'));
+    // get message input
+    const msgInput = await msgForm.findElement(By.name('message-input-text'));
+    // set text and send by pressing enter
+    await msgInput.sendKeys(messages[0],Key.RETURN);
+    //set text and press send button
+    await msgInput.sendKeys(messages[1]);
+    await msgForm.findElement(By.className('btn-submit')).click();
+});
+it('should find message',async () => {
+
+    const chatContainer = await driver.findElement(By.className('chat-container'));
+    const messageContainer = await chatContainer.findElement(By.className('messages'));
+    const messageElements = await messageContainer.findElements(By.className('msg-container'));
+
+    let foundMessage = false;
+    for (let i = messageElements.length - 1; i >= 0; i--) {
+        // get message item
+        const messageItem = await messageElements[i].findElement(By.className('content'));
+        const text = await messageItem.getText();
+
+        if(text.includes(messages[0]))
+            foundMessage = true;
+    }
+    expect(foundMessage).toEqual(true);
 });
 
 afterAll(async () => {
